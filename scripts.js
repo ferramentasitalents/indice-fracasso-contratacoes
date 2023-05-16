f62fdc3592866ef9921d1eda36567d14d2f0dffc
ferramentasitalents <133061937+ferramentasitalents@users.noreply.github.com>
2023-05-16 14:42:40 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
+function calculateIndex() {
+  const fracassadas = parseFloat(document.getElementById("fracassadas").value);
+  const total = parseFloat(document.getElementById("total").value);
+  const index = (fracassadas / total) * 100;
+  document.getElementById("result").innerHTML = `Índice de Fracasso de Novas Contratações: ${index.toFixed(2)}%`;
+}
 
